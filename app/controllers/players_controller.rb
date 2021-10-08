fef7ad397d602a35743bcdb40155a19e4cb8bc43
yamadaab <s.yamada.1125@gmail.com>
2021-10-08 15:37:49 +0900

    投手の成績表示作成完了

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,8 +5,5 @@ class PlayersController < ApplicationController
 
   def show
     @player = Player.find(params[:id])
-
-    # @player_stat = player.pitcher_stat if player.pitcher?
-    # @player_stat = player.fielder_stat if player.fielder?
   end
 end
